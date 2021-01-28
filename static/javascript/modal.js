6ff1e1fc6d8d71ee7c8a1f511df9cdaf78a2d569
Soham <sohamnandi77@gmail.com>
2021-01-28 18:57:17 +0530

    Added add account functionality

--- static/javascript/modal.js
+++ static/javascript/modal.js
@@ -1,6 +1,8 @@
 let modalBtn = document.querySelector(".addacc");
 let modalBg = document.querySelector(".modal-bg");
 let modalClose = document.querySelector(".btn-cancel");
+let modalAdd = document.querySelector(".btn-add");
+let noteBody = document.querySelector(".note-table");
 
 // open modal
 modalBtn.addEventListener("click", function () {
@@ -10,5 +12,43 @@ modalBtn.addEventListener("click", function () {
 // close modal
 modalClose.addEventListener("click", function (e) {
   e.preventDefault();
+  let newAcc = document.querySelector(".new-acc");
+  let newEmail = document.querySelector(".new-email");
+  let newUsername = document.querySelector(".new-username");
+  let newPassword = document.querySelector(".new-password");
+
+  newAcc.value = "";
+  newEmail.value = "";
+  newUsername.value = "";
+  newPassword.value = "";
+
   modalBg.classList.remove("bg-active");
 });
+
+// add account
+modalAdd.addEventListener("click", function (e) {
+  let newAcc = document.querySelector(".new-acc");
+  let newEmail = document.querySelector(".new-email");
+  let newUsername = document.querySelector(".new-username");
+  let newPassword = document.querySelector(".new-password");
+  let note = document.createElement("tr");
+  note.innerHTML = `  
+  <td class="acc-name">${newAcc.value}</td>
+  <td class="acc-username">${newEmail.value}</td>
+  <td class="acc-email">${newUsername.value}</td>
+  <td class="acc-password">${newPassword.value}</td>
+  <td class="acc-icon">
+  <i class="fas fa-edit"></i>
+  <i class="fas fa-trash"></i>
+  </td>
+  `;
+  newAcc.value = "";
+  newEmail.value = "";
+  newUsername.value = "";
+  newPassword.value = "";
+  modalBg.classList.remove("bg-active");
+  noteBody.appendChild(note);
+  e.preventDefault();
+});
+
+// delete account
